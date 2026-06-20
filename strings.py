fe04acd8cb21d77ad1ebf6c5df8e2f7310559832
tulasi <kosuritulasi38@gmail.com>
2026-06-20 13:51:24 +0530

    updated string learnings

--- strings.py
+++ strings.py
@@ -1,2 +1,7 @@
 name = "Tulasi" #Here we are storing string value into name variable.
 print(name)
+
+name='Tulasi' # Here we are accessing string value into index variable.
+print(name[0])
+print(name[1])
+print(name[2])
